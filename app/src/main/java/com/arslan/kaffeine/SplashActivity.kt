ad711a61aeb119e74083cb9b3d2220e2ba3f9259
mearslanahmed <mearslanahmed@gmail.com>
2025-11-26 19:17:00 +0500

    Project Setup and Made Splash Screen Module

---
+++ app/src/main/java/com/arslan/kaffeine/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.arslan.kaffeine
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.arslan.kaffeine.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
