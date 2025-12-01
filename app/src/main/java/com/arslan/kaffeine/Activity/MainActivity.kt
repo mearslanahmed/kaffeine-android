3203deba6b5fe3a4e4fd8762832acf2a3377ba6b
mearslanahmed <mearslanahmed@gmail.com>
2025-12-01 18:02:58 +0500

    Implemented Cart activity backend login and added navigation in bottom menu

--- app/src/main/java/com/arslan/kaffeine/Activity/MainActivity.kt
+++ app/src/main/java/com/arslan/kaffeine/Activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.arslan.kaffeine.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -28,6 +29,13 @@ class MainActivity : AppCompatActivity() {
         initBanner()
         initCategory()
         initPopular()
+        initBottomMenu()
+    }
+
+    private fun initBottomMenu() {
+        binding.cartBtn.setOnClickListener {
+            startActivity(Intent(this, CartActivity::class.java))
+        }
     }
 
     private fun initPopular() {
